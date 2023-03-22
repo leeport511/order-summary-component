a60082a78fce866d7af9f05205267819b8c695d7
leeport511 <leonardoandres.portillo@gmail.com>
2023-03-22 19:41:42 -0300

    fixed annual plan section

--- src/components/OrderSummaryComponent.jsx
+++ src/components/OrderSummaryComponent.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export const OrderSummaryComponent = () => {
     return (
         <section className="flex h-screen items-center justify-center bg-pale-blue ">
-            <article className="w-[80%] bg-white rounded-3xl ">
+            <article className="w-[80%] rounded-3xl bg-white ">
                 <div className="mb-4">
                     <img
                         src="./images/illustration-hero.svg"
@@ -13,25 +13,43 @@ export const OrderSummaryComponent = () => {
                 </div>
                 <div className="p-8">
                     <div className="mb-8 text-center">
-                        <h3 className="font-black text-2xl mb-4">Order Summary</h3>
+                        <h3 className="mb-4 text-2xl font-black">
+                            Order Summary
+                        </h3>
                         <p className="text-desaturated-blue">
-                            You can now listen to millions of songs, audiobooks, and
-                            podcasts on any device anywhere you like!
+                            You can now listen to millions of songs, audiobooks,
+                            and podcasts on any device anywhere you like!
                         </p>
                     </div>
-                    <div className="mb-8 flex  bg-very-pale-blue p-2 rounded-xl py-6">
-                        <img src="./images/icon-music.svg" alt="music icon" className="ml-3" />
-                        <div className="ml-4">
-                            <p className="font-black">Annual Plan</p>
-                            <p className="text-desaturated-blue">$59.99/year</p>
+                    <div className="mb-8 flex justify-between rounded-xl bg-very-pale-blue p-2 py-6">
+                        <div className="flex ml-4">
+                            <img
+                                src="./images/icon-music.svg"
+                                alt="music icon"
+                                className="mr-3"
+                            />
+                            <div>
+                                <p className="font-black">Annual Plan</p>
+                                <p className="text-desaturated-blue">
+                                    $59.99/year
+                                </p>
+                            </div>
                         </div>
-                        <a href="#" className="flex items-center ml-16 underline text-bright-blue font-black hover:text-dark-blue hover:no-underline ">Changue</a>
+                        <a
+                            href="#"
+                            className="flex items-center font-black mr-4 text-bright-blue underline hover:text-dark-blue hover:no-underline "
+                        >
+                            Changue
+                        </a>
                     </div>
                     <div className="flex flex-col">
-                        <button className="mb-4 bg-bright-blue text-white hover:bg-dark-blue p-4 rounded-xl shadow-xl shadow-desaturated-blue font-bold">Proceed to Payment</button>
-                        <button className="p-4 font-bold text-desaturated-blue hover:text-black hover:font-black">Cancel Order</button>
+                        <button className="mb-4 rounded-xl bg-bright-blue p-4 font-bold text-white shadow-xl shadow-desaturated-blue hover:bg-dark-blue">
+                            Proceed to Payment
+                        </button>
+                        <button className="p-4 font-bold text-desaturated-blue hover:font-black hover:text-black">
+                            Cancel Order
+                        </button>
                     </div>
-
                 </div>
             </article>
         </section>
